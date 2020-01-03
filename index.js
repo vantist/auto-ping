f82c124b44203dcba191b195efdeadf940d26904
Vantist <s7790428@gmail.com>
2020-01-03 18:46:00 +0800

    [add]  add auto ping

---
+++ index.js
@@ -0,0 +1,28 @@
+'use strict';
+
+const express = require('express');
+const request = require('request');
+
+// create LINE SDK config from env variables
+const config = {
+  pingServer: process.env.PING_TARGET_SERVER
+};
+
+const app = express();
+
+app.get('/ping', (req, res) => {
+  res.status(200).end();
+});
+
+// listen on port
+const port = process.env.PORT || 8080;
+app.listen(port, () => {
+  console.log(`listening on ${port}`);
+});
+
+cron.schedule('0 10 * * * *', () => {
+  console.log('auto ping to ${config.pingServer}');
+  request.get(config.pingServer, (err, response) => {
+    console.log('auto ping to ${config.pingServer} done');
+  });
+});
